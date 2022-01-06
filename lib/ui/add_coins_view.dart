c2e12b0379bee9902b70e25bd052e3a69b0d9f38
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-07 00:50:25 +0100

    new page to add new data

---
+++ lib/ui/add_coins_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddView extends StatefulWidget {
+  const AddView({Key key}) : super(key: key);
+
+  @override
+  _AddViewState createState() => _AddViewState();
+}
+
+class _AddViewState extends State<AddView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add a new oin"),
+      ),
+    );
+  }
+}
