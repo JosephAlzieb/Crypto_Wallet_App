90f6da381fa92d2cb93c0b187be534fbb05c3c80
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-07 00:15:32 +0100

    create the home page. here we will see the currencies after successful login

---
+++ lib/ui/home_view.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text("My currencies"),
+      ),
+    );
+  }
+}
