fa17989f92ccc7cd4b4aa501c81950bc91fc8916
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-07 00:12:57 +0100

    add Firebase.initializeApp()

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:crypto_wallet/ui/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
