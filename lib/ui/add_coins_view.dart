5280d81e31e2fa29f4dd0636577ee8f78298dc6f
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-07 13:06:12 +0100

    finish the add coin view.

--- lib/ui/add_coins_view.dart
+++ lib/ui/add_coins_view.dart
@@ -1,3 +1,5 @@
+import 'package:crypto_wallet/net/flutterFire.dart';
+import 'package:crypto_wallet/widgets.dart';
 import 'package:flutter/material.dart';
 
 class AddView extends StatefulWidget {
@@ -8,11 +10,53 @@ class AddView extends StatefulWidget {
 }
 
 class _AddViewState extends State<AddView> {
+  List<String> coins = ["Bitcoin", "Sol", "Ether", "Link", "Dot"];
+
+  String dropdownValue = "Bitcoin";
+  TextEditingController _Amount = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Add a new oin"),
+        backgroundColor: Colors.blueAccent,
+        title: const Text("Add a new coin"),
+      ),
+      body: Container(
+        color: Colors.blueAccent,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              DropdownButton(
+                dropdownColor: Colors.blueAccent,
+                iconEnabledColor: Colors.white,
+                items: coins
+                    .map<DropdownMenuItem<String>>((coin) => DropdownMenuItem(
+                          child: Text(
+                            coin,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                          value: coin,
+                        ))
+                    .toList(),
+                value: dropdownValue,
+                onChanged: (value) {
+                  setState(() {
+                    dropdownValue = value;
+                  });
+                },
+              ),
+              getTextFormField(_Amount, "", "Coin amount",),
+              getTextButton("Add Coin", context, () {
+                addCoin(dropdownValue, _Amount.text);
+                Navigator.pop(context);
+              })
+            ],
+          ),
+        ),
       ),
     );
   }
